e8571dbe59f97be0464a469c0a36d4dae75d0cbe
zkkidman <zkkobe@outlook.com>
2022-12-06 11:52:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,6 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/WYC-2020/openwrt-passwall.git' >>feeds.conf.default
+echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
 echo 'src-git NanoHatOLED https://github.com/vinewx/NanoHatOLED.git' >>feeds.conf.default
+ 
